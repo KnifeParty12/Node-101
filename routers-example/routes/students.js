906079d4d497005b163e19e683d51a2f16dfd6f6
KnifeParty12 <flashfunk01@gmail.com>
2019-03-20 01:19:36 +0530

    Routing in Express and creating routes
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

---
+++ routers-example/routes/students.js
@@ -0,0 +1,13 @@
+const route = require('express').Router();
+
+let students = [
+    {name: "Rahul" , college: "DTU" , year: "I"},
+    {name: "Neha" , college: "IIIT" , year: "II"},
+    {name: "Nikhil" , college: "NSIT" , year: "III"},
+    {name: "Saksham" , college: "IIT" , year: "IV"}
+];
+
+route.get('/' , (req,res)=> res.send(students));
+route.get('/:id' , (req,res)=>res.send(students[req.params.id]))
+
+module.exports = route;
